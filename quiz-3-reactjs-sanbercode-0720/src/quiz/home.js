65c882870294122e024bdefc2a96b8f6a70c7c16
maswijay <martenzitic@gmail.com>
2020-08-10 08:10:03 +0700

    modif dikit

--- quiz-3-reactjs-sanbercode-0720/src/quiz/home.js
+++ quiz-3-reactjs-sanbercode-0720/src/quiz/home.js
@@ -26,9 +26,9 @@ class Home extends Component{
                     {this.state.movie.sort().map(movie => 
                         <>
                         <h2 key={movie.id}>{movie.title}</h2>
-                        <h3>Rating {movie.rating}</h3>
-                        <h3>Duration: {movie.duration/60}jam</h3>
-                        <h3>Genre: {movie.genre}</h3>
+                        <h4>Rating {movie.rating}</h4>
+                        <h4>Duration: {movie.duration/60}jam</h4>
+                        <h4>Genre: {movie.genre}</h4>
                         <p><strong>Deskripsi: </strong>{movie.description}</p>
                         
                         </>)}
